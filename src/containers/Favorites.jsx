e1a4377179c5a95f3466ee882310dc1e2b397cbc
Sean Ghods <137848705+seanghods@users.noreply.github.com>
2023-11-20 04:17:50 -0800

    fix favorites not logged in

--- src/containers/Favorites.jsx
+++ src/containers/Favorites.jsx
@@ -1,7 +1,19 @@
 import { StreamList } from '../components/';
 import { useEffect } from 'react';
+import useStream from '../Context/useStream';
 
 export default function Favorites({ fetchStreamers, fetchMoreStreamers }) {
+  const { loggedIn } = useStream();
+  if (!loggedIn) {
+    return (
+      <div className="flex items-center justify-center flex-col gap-24">
+        <h1 className="font-logo text-3xl">Error!</h1>
+        <p className="font-game italic text-lg">
+          Please login to visit favorites
+        </p>
+      </div>
+    );
+  }
   useEffect(() => {
     fetchStreamers(null, null, true);
   }, [fetchStreamers]);
